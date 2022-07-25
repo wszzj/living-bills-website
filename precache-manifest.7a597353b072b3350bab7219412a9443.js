2e1cd8ae6d6162c844ceaee76d10269698b2a6af
zzj <1005978400@qq.com>
2022-07-25 22:48:51 +0800

    deploy

---
+++ precache-manifest.7a597353b072b3350bab7219412a9443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdad960f60aaa3fb2485d2f829b3c779",
+    "url": "/living-bills-website/QRcode.png"
+  },
+  {
+    "revision": "42d33cf902b4f7620c80",
+    "url": "/living-bills-website/css/app.d1b254a6.css"
+  },
+  {
+    "revision": "9b6dacfbb347b475a08068639d8f2551",
+    "url": "/living-bills-website/index.html"
+  },
+  {
+    "revision": "42d33cf902b4f7620c80",
+    "url": "/living-bills-website/js/app.3f3083bf.js"
+  },
+  {
+    "revision": "cb313f5b4e03b4b55d61",
+    "url": "/living-bills-website/js/chunk-vendors.b2659fe2.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/living-bills-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/living-bills-website/robots.txt"
+  }
+]);
